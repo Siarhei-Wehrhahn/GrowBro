ca9ed1ea42d45b735de0d886572865857ff1bf56
Sergej <sergei1997.sw@gmail.com>
2024-07-16 10:10:48 +0200

    Initial Commit

---
+++ GrowBro/GrowBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowBroApp.swift
+//  GrowBro
+//
+//  Created by Siarhei Wehrhahn on 16.07.24.
+//
+
+import SwiftUI
+
+@main
+struct GrowBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
